4596454461e591debf7de09fcc2bf77393f83c50
Martin Mulder <m.j.mulder@provinciegroningen.nl>
2019-10-08 14:22:30 +0200

    fix indentation

--- src/SsllabsScanner.php
+++ src/SsllabsScanner.php
@@ -1,84 +1,83 @@
-	<?php
+<?php
+namespace SSLLabs\Laravel;
 
-	namespace SSLLabs\Laravel;
 
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+use Andyftw\SSLLabs\Api;
 
-	use Illuminate\Container\Container;
-	use Illuminate\Support\Facades\Log;
-	use Illuminate\Support\ServiceProvider;
-	use Andyftw\SSLLabs\Api;
+class SsllabsScanner
+{
 
-	class SsllabsScanner
-	{
+	private $ssllabsApi = null;
 
-		private $ssllabsApi = null;
+	private $url = null;
+	private $state = "NEW";
+	private $result = null;
 
-		private $url = null;
-		private $state = "NEW";
-		private $result = null;
+    public function __construct(Api $ssllabsApi)
+    {
+    	Log::debug('Constructing ssllabsscanner');
+    	$this->ssllabsApi = $ssllabsApi;
+    } 
 
-	    public function __construct(Api $ssllabsApi)
-	    {
-	    	Log::debug('Constructing ssllabsscanner');
-	    	$this->ssllabsApi = $ssllabsApi;
-	    } 
+    public function scan($url)
+    {
+        $this->url = $url;
+        try {
+                // analyze($host, $publish, $startNew, $fromCache, $maxAge, $all, $ignoreMismatch )
+                // @see: https://github.com/ssllabs/ssllabs-scan/blob/stable/ssllabs-api-docs.md#invoke-assessment-and-check-progress
+                $this->ssllabsApi->analyze($url, false, true, false, 0, null, 1);
+                
+                // Give SSL ten seconds to start
+                sleep(10);
 
-	    public function scan($url)
-	    {
-	        $this->url = $url;
-	        try {
-	                // analyze($host, $publish, $startNew, $fromCache, $maxAge, $all, $ignoreMismatch )
-	                // @see: https://github.com/ssllabs/ssllabs-scan/blob/stable/ssllabs-api-docs.md#invoke-assessment-and-check-progress
-	                $this->ssllabsApi->analyze($url, false, true, false, 0, null, 1);
-	                
-	                // Give SSL ten seconds to start
-	                sleep(10);
+                $currentStatus = "NEW";         
 
-	                $currentStatus = "NEW";         
+                while ($this->state != 'READY') {
+                    // Fetch data from api
+                    $host = $this->ssllabsApi->analyze($url);
+                    $currentStatus = $host->getStatus();
+                    $currentMessage = $host->getStatusMessage();
 
-	                while ($this->state != 'READY') {
-	                    // Fetch data from api
-	                    $host = $this->ssllabsApi->analyze($url);
-	                    $currentStatus = $host->getStatus();
-	                    $currentMessage = $host->getStatusMessage();
+                    foreach($host->getEndpoints() as $endpoint) {
+                            Log::debug('endpoint: ' . $endpoint->getIpAddress() . ' status details: ' . $endpoint->getStatusDetails());
+                    }
+                    // update the current state
+                    $this->state = $currentStatus;
+                    // SSLLabs isn't ready
+                sleep(10);
+                }
 
-	                    foreach($host->getEndpoints() as $endpoint) {
-	                            Log::debug('endpoint: ' . $endpoint->getIpAddress() . ' status details: ' . $endpoint->getStatusDetails());
-	                    }
-	                    // update the current state
-	                    $this->state = $currentStatus;
-	                    // SSLLabs isn't ready
-	                sleep(10);
-	                }
+                // update the current state
+                $this->state = $currentStatus;
+                $this->result = $this->ssllabsApi->analyze($url);
+                return $this->result;
+            } catch(\Exception $e) {
+                Log::error('SSL scan error for url: ' . $url . " message: " . $e->getMessage());
+                $this->state = "FAILED";
+                return false;
+            }
+    }
 
-	                // update the current state
-	                $this->state = $currentStatus;
-	                $this->result = $this->ssllabsApi->analyze($url);
-	                return $this->result;
-	            } catch(\Exception $e) {
-	                Log::error('SSL scan error for url: ' . $url . " message: " . $e->getMessage());
-	                $this->state = "FAILED";
-	                return false;
-	            }
-	    }
+    public function endpointData($url, $endpoint)
+    {
+    	return $this->ssllabsApi->getEndpointData($url, $endpoint);
+    }
 
-	    public function endpointData($url, $endpoint)
-	    {
-	    	return $this->ssllabsApi->getEndpointData($url, $endpoint);
-	    }
+    public function getState()
+    {
+    	return $this->state;
+    }
 
-	    public function getState()
-	    {
-	    	return $this->state;
-	    }
+    public function getUrl() 
+    {
+    	return $this->url;
+    }
 
-	    public function getUrl() 
-	    {
-	    	return $this->url;
-	    }
-
-	    public function getResult()
-	    {
-	    	return $this->result;
-	    }
-	}
+    public function getResult()
+    {
+    	return $this->result;
+    }
+}
